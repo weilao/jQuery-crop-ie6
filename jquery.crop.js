a305636d234a46265450b44651403a69012986be
Adrien Gibrat <adrien.gibrat@gmail.com>
2014-04-23 01:05:20 +0200

    factorize node creation + formatting code

--- jquery.crop.js
+++ jquery.crop.js
@@ -5,13 +5,18 @@
  */
 ( function ( $, undefined ) {
 	var namespace = 'crop' // IE sucks
-	    , plugin  = function ( image, options ) {
-			var self  = this
-			    , img = new Image();
-			this.$image  = $( image ).wrap( '<div class="' + namespace + 'Frame"/>' ) // wrap image in frame;
-			this.options = $.extend({}, this.options, {
-				width    : this.$image.attr('width')
-				, height : this.$image.attr('height')
+		, plugin  = function ( image, options ) {
+			var self   = this
+				, img  = new Image()
+				, node = function ( tag, classname ) {
+					return $( '<' + tag + '/>', { 'class' : namespace + classname.charAt( 0 ).toUpperCase() + classname.slice( 1 ) } );
+				}
+			;
+			this.$image  = $( image ).wrap( node( 'div', 'frame' ) ) // wrap image in frame;
+			this.options = $.extend( {}, this.options, {
+				width    : this.$image.attr(  'width' )
+				, height : this.$image.attr( 'height' )
+				, style  : this.$image.css( [ 'display', 'left', 'top', 'width', 'height' ] )
 			}, options );
 			this.$frame  = this.$image.parent();
 			this.$image
@@ -33,33 +38,37 @@
 						}, $.proxy( self.drag, self ) )
 						.on( 'mouseup.' + namespace, function () {
 							$( document ).off( '.' + namespace )
-						} );
+						} )
+					;
 				} )
 				.on( 'load.' + namespace, function () {
-					if (img.naturalWidth === undefined) {
-						img.src = self.$image.attr('src'); // should not need to wait image load event as src is loaded
-						self.$image.prop('naturalWidth', img.width);
-						self.$image.prop('naturalHeight', img.height);
+					if ( img.naturalWidth === undefined ) {
+						img.src = self.$image.attr( 'src' ); // should not need to wait image load event as src is loaded
+						self.$image.prop( 'naturalWidth', img.width );
+						self.$image.prop( 'naturalHeight', img.height );
 					}
-					var width    = self.$image.prop('naturalWidth')
-					    , height = self.$image.prop('naturalHeight');
+					var width    = self.$image.prop( 'naturalWidth' )
+					    , height = self.$image.prop( 'naturalHeight' );
 					self.minPercent = Math.max( width ? self.options.width / width : 1, height ? self.options.height / height : 1 );
 					self.focal      = { x : Math.round( width / 2 ), y : Math.round( height / 2 ) };
 					self.zoom( self.minPercent );
 					self.$image.fadeIn( 'fast' ); //display image now that it is loaded
 				} )
-				.trigger('load.' + namespace); // init even if image is already loaded
+				.trigger( 'load.' + namespace ) // init even if image is already loaded
+			;
 			this.$frame
 				.css( { width : this.options.width, height : this.options.height } )
-				.append( this.options.loading )
-				.hover(function(){self.$frame.toggleClass('hover');});
+				.append( node( 'span', 'loading' ).append( this.options.loading ) )
+				.hover( function () { self.$frame.toggleClass( 'hover' ); } )
+			;
 			if ( this.options.controls !== false )
 				this.$frame
-					.append( $( '<div/>', { 'class' : namespace + 'Controls' } )
-						.append( $( '<span/>' ).append( this.options.controls ) )
-						.append( $( '<a/>', { 'class' : namespace + 'ZoomIn' } ).on( 'click.' + namespace, $.proxy( this.zoomIn, this ) ) )
-						.append( $( '<a/>', { 'class' : namespace + 'ZoomOut' } ).on( 'click.' + namespace, $.proxy( this.zoomOut, this ) ) )
-					);
+					.append( node( 'div', 'controls' )
+						.append( node( 'span', 'text' ).append( this.options.controls ) )
+						.append( node( 'a', 'zoomIn' ).on( 'click.' + namespace, $.proxy( this.zoomIn, this ) ) )
+						.append( node( 'a', 'zoomOut' ).on( 'click.' + namespace, $.proxy( this.zoomOut, this ) ) )
+					)
+				;
 		}
 	;
 	$[ namespace ] = $.extend( plugin
@@ -76,8 +85,8 @@
 			}
 			, zoom    : function ( percent ) {
 				this.percent = Math.max( this.minPercent, Math.min( this.options.stretch, percent ) );
-				this.$image.width( Math.ceil( this.$image.prop('naturalWidth') * this.percent ) );
-				this.$image.height( Math.ceil( this.$image.prop('naturalHeight') * this.percent ) );
+				this.$image.width( Math.ceil( this.$image.prop( 'naturalWidth' ) * this.percent ) );
+				this.$image.height( Math.ceil( this.$image.prop( 'naturalHeight' ) * this.percent ) );
 				this.$image.css( { 
 					left  : plugin.fill( - Math.round( this.focal.x * this.percent - this.options.width / 2 ), this.$image.width(), this.options.width )
 					, top : plugin.fill( - Math.round( this.focal.y * this.percent - this.options.height / 2 ), this.$image.height(), this.options.height )
@@ -99,8 +108,8 @@
 			}
 			, update  : function () {
 				this.focal = {
-					x   : Math.round( ( this.options.width / 2 - parseInt( this.$image.css( 'left'), 10 ) ) / this.percent )
-					, y : Math.round( ( this.options.height / 2 - parseInt( this.$image.css ('top' ), 10 ) ) / this.percent )
+					x   : Math.round( ( this.options.width / 2 - parseInt( this.$image.css( 'left' ), 10 ) ) / this.percent )
+					, y : Math.round( ( this.options.height / 2 - parseInt( this.$image.css( 'top' ), 10 ) ) / this.percent )
 				};
 				this.$image.trigger( $.Event( 'crop', {
 					cropX     : - Math.floor( parseInt( this.$image.css( 'left' ), 10 ) / this.percent )
